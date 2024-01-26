3fcff9f0ce3bae0a93cd45d77a3a5bfaba4d1baf
YHaziri <yll.haziri09@gmail.com>
2024-01-26 20:52:30 +0100

    Shuma e dy numrave
    Shuma e dy numrave

---
+++ klasa.java
@@ -0,0 +1,9 @@
+public class klasa {
+    public static void main(String[] args) {
+        System.out.println(metoda1(3,5));
+    }
+    public static int metoda1(int a, int b){
+        return a+b;
+
+    }
+}
