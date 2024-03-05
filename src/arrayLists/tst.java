fc4d95f952741bed8bbd08a53513de4bc7cf7889
KastriotBlakaj <lotiblakaj123@gmail.com>
2024-03-05 20:37:50 +0100

    array list

--- src/arrayLists/tst.java
+++ src/arrayLists/tst.java
@@ -1,27 +1,42 @@
 package src.arrayLists;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class tst {
     public static void main(String[] args) {
-        ArrayList<String> list = new ArrayList<>();
+        listInt();
+        listString();
+    }
 
-        //Shtimi i antarve ne ArrayList
-        list.add("Apple");
-        list.add("Banana");
-        list.add("Orange");
-        list.add("Mango");
+    public static void listString(){
+        List<String> emrat = new ArrayList<>();
 
-        //Iterrimi ne list
-        for(String fruts :list){
-            System.out.println(fruts);
+        //emra e klases
+        emrat.add("Yllin");
+        emrat.add("Kastrioti");
+        emrat.add("vetoni");
+        System.out.println(emrat.get(2));
+        System.out.println("Gjateisa: "+emrat.size());
+        System.out.println("Antari i pare : "+emrat.getFirst());
+        System.out.println("Antari i pare : "+emrat.getLast());
+        emrat.remove(1);
+        System.out.println(emrat);
+    }
+
+    public static void listInt(){
+        List<Integer> mosha = new ArrayList<>();
+
+        //SHTIMI I VLERABVE
+        mosha.add(25);
+        mosha.add(15);
+        mosha.add(45);
+        mosha.add(65);
+//        System.out.println(mosha);
+        for (int i = 0; i < mosha.toArray().length; i++) {
+            System.out.println(mosha.toArray()[i]);
         }
 
-        //Modifikimi i antareve ne list
-        list.set(1,"Grapes");
 
-        //Largimi i nje antari nga ArrayLista
-        list.remove(2);
-        System.out.println("Gjatesina: "+ list.toArray().length);
     }
 }
